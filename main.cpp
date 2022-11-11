9cb3f3b94ae3e2afbf99e3f776e363833cbe5405
Yarik207 <100360508+Yarik207@users.noreply.github.com>
2022-11-11 18:05:29 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ txDisableAutoPause();
 txTextCursor (false);
 
     int COUNT_BTN = 5;
-    int COUNT_PICTURE = 15;
+    int COUNT_PICTURE = 36;
     int select = -1;
     bool mouse_free = false;
     char str[100];
@@ -81,40 +81,89 @@ txTextCursor (false);
 
     //Массив иконок-меню левых
     Pictures menuPictures[COUNT_PICTURE];
-    menuPictures[0] = {50, 100, txLoadImage ("Pictures/Нос/nose1.bmp"), 100, 100, 100, 100, "Нос", false};
-    menuPictures[1] = {50, 200, txLoadImage ("Pictures/Нос/nose2.bmp"), 100, 100, 100, 100, "Нос", false};
-    menuPictures[2] = {50, 300, txLoadImage ("Pictures/Нос/nose3.bmp"), 100, 100, 100, 100, "Нос", false};
-    menuPictures[3] = {50, 100, txLoadImage ("Pictures/Глаза/eyes1.bmp"), 100, 35, 100, 35, "Глаза", false};
-    menuPictures[4] = {50, 200, txLoadImage ("Pictures/Глаза/eyes2.bmp"), 100, 34, 100, 34, "Глаза", false};
-    menuPictures[5] = {50, 300, txLoadImage ("Pictures/Глаза/eyes3.bmp"), 100, 61, 100, 61, "Глаза", false};
-    menuPictures[6] = {50, 100, txLoadImage ("Pictures/Овал лица/face1M.bmp"), 94, 100, 94, 100, "Лицо", false};
-    menuPictures[7] = {50, 200, txLoadImage ("Pictures/Овал лица/face2M.bmp"), 97, 100, 97, 100, "Лицо", false};
-    menuPictures[8] = {50, 300, txLoadImage ("Pictures/Овал лица/face3M.bmp"), 70, 100, 70, 100, "Лицо", false};
-    menuPictures[9] = {50, 100, txLoadImage ("Pictures/Челка/Volosi1M.bmp"), 80, 100, 80, 100, "Волосы", false};
-    menuPictures[10] ={50, 200, txLoadImage ("Pictures/Челка/Volosi2M.bmp"), 87, 100, 87, 100, "Волосы", false};
-    menuPictures[11] ={50, 300, txLoadImage ("Pictures/Челка/Volosi3M.bmp"), 100, 97, 100, 97, "Волосы", false};
-    menuPictures[12] ={50, 100, txLoadImage ("Pictures/Губы/Gubi1.bmp"), 100, 100, 100, 100, "Губы", false};
-    menuPictures[13] ={50, 200, txLoadImage ("Pictures/Губы/Gubi2.bmp"), 100, 100, 100, 100, "Губы", false};
-    menuPictures[14] ={50, 300, txLoadImage ("Pictures/Губы/Gubi3.bmp"), 100, 100, 100, 100, "Губы", false};
+    menuPictures[0] = {50, 100, txLoadImage ("Pictures/Нос/nose1.bmp"), 100, 100, 50, 50, "Нос", false};
+    menuPictures[1] = {50, 200, txLoadImage ("Pictures/Нос/nose2.bmp"), 100, 100, 50, 50, "Нос", false};
+    menuPictures[2] = {50, 300, txLoadImage ("Pictures/Нос/nose3.bmp"), 100, 125, 50, 50, "Нос", false};
+    menuPictures[3] = {50, 400, txLoadImage ("Pictures/Нос/nose4.bmp"), 59, 100, 50, 50,  "Нос", false};
+    menuPictures[4] = {50, 500, txLoadImage ("Pictures/Нос/nose5.bmp"), 100, 116, 50, 50, "Нос", false};
+    menuPictures[5] = {50, 600, txLoadImage ("Pictures/Нос/nose6.bmp"), 62, 100, 50, 50,  "Нос", false};
 
-    //Массив иконок-меню центральных
+    menuPictures[6]  = {50, 100, txLoadImage ("Pictures/Овал лица/face1M.bmp"), 94, 100, 94, 100,  "Лицо", false};
+    menuPictures[7]  = {50, 200, txLoadImage ("Pictures/Овал лица/face2M.bmp"), 97, 100, 94, 100,  "Лицо", false};
+    menuPictures[8]  = {50, 300, txLoadImage ("Pictures/Овал лица/face3M.bmp"), 70, 100, 94, 100,  "Лицо", false};
+    menuPictures[9]  = {50, 400, txLoadImage ("Pictures/Овал лица/face4M.bmp"), 100, 136, 94, 100, "Лицо", false};
+    menuPictures[10] = {50, 500, txLoadImage ("Pictures/Овал лица/face5M.bmp"), 100, 115, 94, 100, "Лицо", false};
+    menuPictures[11] = {50, 600, txLoadImage ("Pictures/Овал лица/face6M.bmp"), 100, 131, 94, 100, "Лицо", false};
+
+    menuPictures[12] = {50, 100, txLoadImage ("Pictures/Губы/lips1.bmp"), 100, 57, 100, 100, "Губы", false};
+    menuPictures[13] = {50, 200, txLoadImage ("Pictures/Губы/lips2.bmp"), 100, 54, 100, 100, "Губы", false};
+    menuPictures[14] = {50, 300, txLoadImage ("Pictures/Губы/lips3.bmp"), 100, 51, 100, 100, "Губы", false};
+    menuPictures[15] = {50, 400, txLoadImage ("Pictures/Губы/lips4.bmp"), 100, 50, 100, 100, "Губы", false};
+    menuPictures[16] = {50, 500, txLoadImage ("Pictures/Губы/lips5.bmp"), 100, 51, 100, 100, "Губы", false};
+    menuPictures[17] = {50, 600, txLoadImage ("Pictures/Губы/lips6.bmp"), 100, 51, 100, 100, "Губы", false};
+    menuPictures[18] = {50, 700, txLoadImage ("Pictures/Губы/lips7.bmp"), 100, 33, 100, 100, "Губы", false};
+    menuPictures[19] = {150, 100, txLoadImage ("Pictures/Губы/lips8.bmp"), 100, 46, 100, 100, "Губы", false};
+
+    menuPictures[20] = {50, 100, txLoadImage ("Pictures/Женский/Глаза/eyes1G.bmp"), 100, 35, 100, 35, "Глаза", false};
+    menuPictures[21] = {50, 200, txLoadImage ("Pictures/Женский/Глаза/eyes2G.bmp"), 100, 39, 100, 39, "Глаза", false};
+    menuPictures[22] = {50, 300, txLoadImage ("Pictures/Женский/Глаза/eyes3G.bmp"), 100, 37, 100, 37, "Глаза", false};
+    menuPictures[23] = {50, 400, txLoadImage ("Pictures/Женский/Глаза/eyes4G.bmp"), 100, 27, 100, 27, "Глаза", false};
+    menuPictures[24] = {50, 500, txLoadImage ("Pictures/Мужской/Глаза/eyes1M.bmp"), 100, 46, 100, 46, "Глаза", false};
+    menuPictures[25] = {50, 600, txLoadImage ("Pictures/Мужской/Глаза/eyes2M.bmp"), 100, 38, 100, 38, "Глаза", false};
+    menuPictures[26] = {50, 700, txLoadImage ("Pictures/Мужской/Глаза/eyes3M.bmp"), 100, 33, 100, 33, "Глаза", false};
+    menuPictures[27] = {150, 100, txLoadImage ("Pictures/Мужской/Глаза/eyes4M.bmp"), 100, 32, 100, 32, "Глаза", false};
+
+    menuPictures[28] = {50, 100, txLoadImage ("Pictures/Женский/Челка/Hair1G.bmp"), 100, 108, 100, 108, "Волосы", false};
+    menuPictures[29] = {50, 200, txLoadImage ("Pictures/Женский/Челка/Hair2G.bmp"), 100, 102, 100, 102, "Волосы", false};
+    menuPictures[30] = {50, 300, txLoadImage ("Pictures/Женский/Челка/Hair3G.bmp"), 100, 105, 100, 105, "Волосы", false};
+    menuPictures[31] = {50, 400, txLoadImage ("Pictures/Женский/Челка/Hair4G.bmp"), 100, 123, 100, 123, "Волосы", false};
+    menuPictures[32] = {50, 500, txLoadImage ("Pictures/Мужской/Челка/Hair1M.bmp"), 100, 82,  100, 82, "Волосы", false};
+    menuPictures[33] = {50, 600, txLoadImage ("Pictures/Мужской/Челка/Hair2M.bmp"), 100, 90,  100, 90, "Волосы", false};
+    menuPictures[34] = {50, 700, txLoadImage ("Pictures/Мужской/Челка/Hair3M.bmp"), 100, 91,  100, 91, "Волосы", false};
+    menuPictures[35] = {150, 100, txLoadImage ("Pictures/Мужской/Челка/Hair4M.bmp"), 100, 86,  100, 86, "Волосы", false};
+
+    //Массив иконок центральных
     Pictures CentralPictures[COUNT_PICTURE];
-    CentralPictures[0] = {600, 400, txLoadImage ("Pictures/Нос/nose1.bmp"), 100, 100, 200, 200, "Нос", false};
-    CentralPictures[1] = {600, 400, txLoadImage ("Pictures/Нос/nose2.bmp"), 100, 100, 200, 200, "Нос", false};
-    CentralPictures[2] = {600, 400, txLoadImage ("Pictures/Нос/nose3.bmp"), 100, 100, 200, 200, "Нос", false};
-    CentralPictures[3] = {600, 400, txLoadImage ("Pictures/Глаза/eyes1.bmp"), 100, 35, 200, 70, "Глаза", false};
-    CentralPictures[4] = {600, 400, txLoadImage ("Pictures/Глаза/eyes2.bmp"), 100, 34, 200, 68, "Глаза", false};
-    CentralPictures[5] = {600, 400, txLoadImage ("Pictures/Глаза/eyes3.bmp"), 100, 61, 200, 122, "Глаза", false};
-    CentralPictures[6] = {600, 400, txLoadImage ("Pictures/Овал лица/face1M.bmp"), 94, 100, 188, 200, "Лицо", false};
-    CentralPictures[7] = {600, 400, txLoadImage ("Pictures/Овал лица/face2M.bmp"), 97, 100, 197, 100, "Лицо", false};
-    CentralPictures[8] = {600, 400, txLoadImage ("Pictures/Овал лица/face3M.bmp"), 70, 100, 140, 200, "Лицо", false};
-    CentralPictures[9] = {600, 400, txLoadImage ("Pictures/Челка/Volosi1M.bmp"), 80, 100, 160, 200, "Волосы", false};
-    CentralPictures[10] ={600, 400, txLoadImage ("Pictures/Челка/Volosi2M.bmp"), 87, 100, 174, 200, "Волосы", false};
-    CentralPictures[11] ={600, 400, txLoadImage ("Pictures/Челка/Volosi3M.bmp"), 100, 97, 200, 194, "Волосы", false};
-    CentralPictures[12] ={600, 400, txLoadImage ("Pictures/Губы/Gubi1.bmp"), 100, 100, 200, 200, "Губы", false};
-    CentralPictures[13] ={600, 400, txLoadImage ("Pictures/Губы/Gubi2.bmp"), 100, 100, 200, 200, "Губы", false};
-    CentralPictures[14] ={600, 400, txLoadImage ("Pictures/Губы/Gubi3.bmp"), 100, 100, 200, 200, "Губы", false};
+    CentralPictures[0]  =  {600, 200, menuPictures[0].image , menuPictures[0].w, menuPictures[0].h, menuPictures[0].w_scr, menuPictures[0].h_scr, menuPictures[0].category, menuPictures[0].visible};
+    CentralPictures[1]  =  {600, 200, menuPictures[1].image , menuPictures[1].w, menuPictures[1].h, menuPictures[1].w_scr, menuPictures[1].h_scr, menuPictures[1].category, menuPictures[1].visible};
+    CentralPictures[2]  =  {600, 200, menuPictures[2].image , menuPictures[2].w, menuPictures[2].h, menuPictures[2].w_scr, menuPictures[2].h_scr, menuPictures[2].category, menuPictures[2].visible};
+    CentralPictures[3]  =  {600, 200, menuPictures[3].image , menuPictures[3].w, menuPictures[3].h, menuPictures[3].w_scr, menuPictures[3].h_scr, menuPictures[3].category, menuPictures[3].visible};
+    CentralPictures[4]  =  {600, 200, menuPictures[4].image , menuPictures[4].w, menuPictures[4].h, menuPictures[4].w_scr, menuPictures[4].h_scr, menuPictures[4].category, menuPictures[4].visible};
+    CentralPictures[5]  =  {600, 200, menuPictures[5].image , menuPictures[5].w, menuPictures[5].h, menuPictures[5].w_scr, menuPictures[5].h_scr, menuPictures[5].category, menuPictures[5].visible};
 
+    CentralPictures[6]  =  {600, 300, menuPictures[6].image , menuPictures[6].w, menuPictures[6].h, menuPictures[6].w_scr, menuPictures[6].h_scr, menuPictures[6].category, menuPictures[5].visible};
+    CentralPictures[7]  =  {600, 300, menuPictures[7].image , menuPictures[7].w, menuPictures[7].h, menuPictures[7].w_scr, menuPictures[7].h_scr, menuPictures[7].category, menuPictures[6].visible};
+    CentralPictures[8]  =  {600, 300, menuPictures[8].image , menuPictures[8].w, menuPictures[8].h, menuPictures[8].w_scr, menuPictures[8].h_scr, menuPictures[8].category, menuPictures[7].visible};
+    CentralPictures[9]  =  {600, 300, menuPictures[9].image , menuPictures[9].w, menuPictures[9].h, menuPictures[9].w_scr, menuPictures[9].h_scr, menuPictures[9].category, menuPictures[8].visible};
+    CentralPictures[10] = {600, 300, menuPictures[10].image , menuPictures[10].w, menuPictures[10].h, menuPictures[10].w_scr, menuPictures[10].h_scr, menuPictures[10].category, menuPictures[10].visible};
+    CentralPictures[11] = {600, 300, menuPictures[11].image , menuPictures[11].w, menuPictures[11].h, menuPictures[11].w_scr, menuPictures[11].h_scr, menuPictures[11].category, menuPictures[11].visible};
+
+    CentralPictures[12] = {600, 400, menuPictures[12].image , menuPictures[12].w, menuPictures[12].h, menuPictures[12].w_scr, menuPictures[12].h_scr, menuPictures[12].category, menuPictures[12].visible};
+    CentralPictures[13] = {600, 400, menuPictures[13].image , menuPictures[13].w, menuPictures[13].h, menuPictures[13].w_scr, menuPictures[13].h_scr, menuPictures[13].category, menuPictures[13].visible};
+    CentralPictures[14] = {600, 400, menuPictures[14].image , menuPictures[14].w, menuPictures[14].h, menuPictures[14].w_scr, menuPictures[14].h_scr, menuPictures[14].category, menuPictures[14].visible};
+    CentralPictures[15] = {600, 400, menuPictures[15].image , menuPictures[15].w, menuPictures[15].h, menuPictures[15].w_scr, menuPictures[15].h_scr, menuPictures[15].category, menuPictures[15].visible};
+    CentralPictures[16] = {600, 400, menuPictures[16].image , menuPictures[16].w, menuPictures[16].h, menuPictures[16].w_scr, menuPictures[16].h_scr, menuPictures[16].category, menuPictures[16].visible};
+    CentralPictures[17] = {600, 400, menuPictures[17].image , menuPictures[17].w, menuPictures[17].h, menuPictures[17].w_scr, menuPictures[17].h_scr, menuPictures[17].category, menuPictures[17].visible};
+    CentralPictures[18] = {600, 400, menuPictures[18].image , menuPictures[18].w, menuPictures[18].h, menuPictures[18].w_scr, menuPictures[18].h_scr, menuPictures[18].category, menuPictures[18].visible};
+    CentralPictures[19] = {600, 400, menuPictures[19].image , menuPictures[19].w, menuPictures[19].h, menuPictures[19].w_scr, menuPictures[19].h_scr, menuPictures[19].category, menuPictures[19].visible};
+
+    CentralPictures[20] = {600, 500, menuPictures[20].image , menuPictures[20].w, menuPictures[20].h, menuPictures[20].w_scr, menuPictures[20].h_scr, menuPictures[20].category, menuPictures[20].visible};
+    CentralPictures[21] = {600, 500, menuPictures[21].image , menuPictures[21].w, menuPictures[21].h, menuPictures[21].w_scr, menuPictures[21].h_scr, menuPictures[21].category, menuPictures[21].visible};
+    CentralPictures[22] = {600, 500, menuPictures[22].image , menuPictures[22].w, menuPictures[22].h, menuPictures[22].w_scr, menuPictures[22].h_scr, menuPictures[22].category, menuPictures[22].visible};
+    CentralPictures[23] = {600, 500, menuPictures[23].image , menuPictures[23].w, menuPictures[23].h, menuPictures[23].w_scr, menuPictures[23].h_scr, menuPictures[23].category, menuPictures[23].visible};
+    CentralPictures[24] = {600, 500, menuPictures[24].image , menuPictures[24].w, menuPictures[24].h, menuPictures[24].w_scr, menuPictures[24].h_scr, menuPictures[24].category, menuPictures[24].visible};
+    CentralPictures[25] = {600, 500, menuPictures[25].image , menuPictures[25].w, menuPictures[25].h, menuPictures[25].w_scr, menuPictures[25].h_scr, menuPictures[25].category, menuPictures[25].visible};
+    CentralPictures[26] = {600, 500, menuPictures[26].image , menuPictures[26].w, menuPictures[26].h, menuPictures[26].w_scr, menuPictures[26].h_scr, menuPictures[26].category, menuPictures[26].visible};
+    CentralPictures[27] = {600, 500, menuPictures[27].image , menuPictures[27].w, menuPictures[27].h, menuPictures[27].w_scr, menuPictures[27].h_scr, menuPictures[27].category, menuPictures[27].visible};
+
+    CentralPictures[28] = {600, 600, menuPictures[28].image , menuPictures[28].w, menuPictures[28].h, menuPictures[28].w_scr, menuPictures[28].h_scr, menuPictures[28].category, menuPictures[28].visible};
+    CentralPictures[29] = {600, 600, menuPictures[29].image , menuPictures[29].w, menuPictures[29].h, menuPictures[29].w_scr, menuPictures[29].h_scr, menuPictures[29].category, menuPictures[29].visible};
+    CentralPictures[30] = {600, 600, menuPictures[30].image , menuPictures[30].w, menuPictures[30].h, menuPictures[30].w_scr, menuPictures[30].h_scr, menuPictures[30].category, menuPictures[30].visible};
+    CentralPictures[31] = {600, 600, menuPictures[31].image , menuPictures[31].w, menuPictures[31].h, menuPictures[31].w_scr, menuPictures[31].h_scr, menuPictures[31].category, menuPictures[31].visible};
+    CentralPictures[32] = {600, 600, menuPictures[32].image , menuPictures[32].w, menuPictures[32].h, menuPictures[32].w_scr, menuPictures[32].h_scr, menuPictures[32].category, menuPictures[32].visible};
+    CentralPictures[33] = {600, 600, menuPictures[33].image , menuPictures[33].w, menuPictures[33].h, menuPictures[33].w_scr, menuPictures[33].h_scr, menuPictures[33].category, menuPictures[33].visible};
+    CentralPictures[34] = {600, 600, menuPictures[34].image , menuPictures[34].w, menuPictures[34].h, menuPictures[34].w_scr, menuPictures[34].h_scr, menuPictures[34].category, menuPictures[34].visible};
+    CentralPictures[35] = {600, 600, menuPictures[35].image , menuPictures[35].w, menuPictures[35].h, menuPictures[35].w_scr, menuPictures[35].h_scr, menuPictures[35].category, menuPictures[35].visible};
 
 
 
@@ -122,7 +171,7 @@ while(!GetAsyncKeyState(VK_ESCAPE))
 {
     txBegin();
     txSetColor (TX_WHITE);
-    txSetFillColor (TX_YELLOW);
+    txSetFillColor (TX_WHITE);
     txClear();
 
 
@@ -137,7 +186,6 @@ while(!GetAsyncKeyState(VK_ESCAPE))
     {
         drawPicture(CentralPictures[npic]);
     }
-
 
     //Центральных картинок рисование
     for(int npic = 0; npic < COUNT_PICTURE; npic++)
@@ -161,14 +209,11 @@ while(!GetAsyncKeyState(VK_ESCAPE))
         }
     }
 
-
     //Рисование кнопок
     for(int nk=0; nk<5; nk++)
     {
         drawButton(btn[nk]);
     }
-
-
 
    //Видимость левых по категориям кнопок
     for(int nk=0; nk<COUNT_BTN; nk++)
@@ -233,28 +278,18 @@ while(!GetAsyncKeyState(VK_ESCAPE))
             CentralPictures[select].x = txMouseX() - CentralPictures[select].w/2;
             CentralPictures[select].y = txMouseY() - CentralPictures[select].h/2;
         }
-        else
-        {
-            if(txMouseButtons() != 1)
+            else
             {
-            mouse_free = true;
+                if(txMouseButtons() != 1)
+                {
+                    mouse_free = true;
+                }
             }
-
-        }
     }
-
-
-
-
-
 
     txSleep(50);
     txEnd();
 }
-
-
-
-
 
     //Удаление левых
     for(int npic=0;npic <COUNT_PICTURE; npic++)
